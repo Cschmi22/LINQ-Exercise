8b421d635b85ce1379cbe6933e5860f85042f0cf
Caleb Schmidt <cschmi14@gmail.com>
2023-03-13 08:27:31 -0400

    Done

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,15 @@
-﻿namespace LINQ
+﻿using System.Linq;
+
+
+List<string> csLewisBooks = new List<string>() { "Mere Christianity", "Surprised By Joy", "Screwtape Letters" };
+
+csLewisBooks.Add("The Abolotion of Man");
+csLewisBooks.Add("A Grief Observed");
+csLewisBooks.Add("The Great Divorce");
+
+var titleLength = csLewisBooks.OrderBy(name => name.Length);
+
+foreach (var item in titleLength)
 {
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
+    Console.WriteLine(item);
 }
